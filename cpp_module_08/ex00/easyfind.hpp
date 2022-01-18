685eba97de05074ecd761f751155a9d9d8c6b356
Carlos Venkman <cvenkman@si-f3.kzn.21-school.ru>
2022-01-18 12:44:44 +0300

    done 08/ex00

--- cpp_module_08/ex00/easyfind.hpp
+++ cpp_module_08/ex00/easyfind.hpp
@@ -2,17 +2,11 @@
 #define EASYFIND_HPP
 
 #include <iostream>
-#include <vector>
-#include <array>
 
 template<typename T>
-typename T::iterator easyfind(T first, int second) {
+typename T::iterator easyfind(T &first, int second) {
 	typename T::iterator it;
-	// end_it = first.end();
 	it = std::find(first.begin(), first.end(), second);
-	std::cout << *it << std::endl;
-	it++;
-	std::cout << *it << std::endl;
 	if (it == first.end())
 		throw std::exception();
 	return it;
--- cpp_module_08/ex00/main.cpp
+++ cpp_module_08/ex00/main.cpp
@@ -1,22 +1,57 @@
 /* containers */
 
 #include "easyfind.hpp"
+#include <vector>
+#include <array>
+#include <deque>
+#include <list>
+#include <map>
 
 int main(void) {
-	// std::vector<int> arr = {2, 3, 4, 6};
-	std::array<int, 4> arr = {2, 3, 5, 4};
-	std::array<int, 4>::iterator it;
-	try
 	{
-		it = easyfind(arr, 3);
-		// std::cout << *it << std::endl;
-		// ++it;
-		// std::cout << *it << std::endl;
+		std::cout << "test with array: ";
+		std::array<int, 4> arr = {2, 3, 5, 4};
+		std::array<int, 4>::iterator it;
+		try
+		{
+			it = easyfind(arr, 3);
+			std::cout << *it << " ";
+			++it;
+			std::cout << *it << std::endl;
+
+			std::cout << *easyfind(arr, 8) << std::endl;
+		}
+		catch (std::exception &ex)
+		{
+			std::cout << ex.what() << std::endl;
+		}
 	}
-	catch (std::exception &ex)
 	{
-		std::cout << ex.what() << std::endl;
+		std::cout << "test with vector: ";
+		std::vector<int> arr(3, 9);
+		try
+		{
+			std::cout << *easyfind(arr, 9) << std::endl;
+			std::cout << *easyfind(arr, 1231) << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
 	}
-	// ++it;
-	// std::cout << *it << std::endl;
+	{
+		std::cout << "test with deque, list and map: ";
+		std::deque<int> arr;
+		std::list<int> list_arr(5, 1);
+		arr.push_back(4);
+		arr.push_back(-8);
+		arr.push_back(8);
+		try
+		{
+			std::cout << *easyfind(arr, -8) << " ";
+			std::cout << *easyfind(list_arr, 1) << std::endl;
+		}
+		catch(const std::exception&)
+		{}
+	}
 }
